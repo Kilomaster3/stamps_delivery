c1aaef3b27485cf031a6b3725658b94fbcf26e4e
Aliaksei  Hrom <grom.4lek@yandex.by>
2020-09-26 10:15:45 +0300

    Rewrite nameing convention, add getter and setter

--- spec/stamps_spec.rb
+++ spec/stamps_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require './stamps'
 
@@ -6,14 +8,14 @@ RSpec.describe StampsDelivery do
     context 'when price 5' do
       it 'found price of delivery' do
         result = StampsDelivery::WarmUpLevel.new
-        expect(result.sum_of_delivery(5)).to eq({ first: 1, second: 2 })
+        expect(result.sum_of_delivery(5)).to eq({ one_cent_stamp_amount: 1, two_cent_stamp_amount: 2 })
       end
     end
 
     context 'when price 15' do
       it 'found price of delivery' do
         result = StampsDelivery::WarmUpLevel.new
-        expect(result.sum_of_delivery(15)).to eq({ first: 1, second: 7 })
+        expect(result.sum_of_delivery(15)).to eq({ one_cent_stamp_amount: 1, two_cent_stamp_amount: 7 })
       end
     end
 
@@ -21,14 +23,14 @@ RSpec.describe StampsDelivery do
       context 'when price 111' do
         it 'found price of delivery' do
           result = StampsDelivery::FirstLevel.new
-          expect(result.sum_of_delivery(111)).to eq({ first: 2, second: 21 })
+          expect(result.sum_of_delivery(111)).to eq({ three_cent_stamp_amount: 2, five_cent_stamp_amount: 21 })
         end
       end
 
       context 'when price 25' do
         it 'found price of delivery' do
           result = StampsDelivery::FirstLevel.new
-          expect(result.sum_of_delivery(25)).to eq({ first: 0, second: 5 })
+          expect(result.sum_of_delivery(25)).to eq({ three_cent_stamp_amount: 0, five_cent_stamp_amount: 5 })
         end
       end
     end
--- stamps.rb
+++ stamps.rb
@@ -1,72 +1,86 @@
+# frozen_string_literal: true
+
 require 'pry'
 
 module StampsDelivery
-  class WarmUpLevel
-    ONEPENNY = 'однокопеечных марок'.freeze
-    TWOPENNY = 'двухкопеечных марок'.freeze
+  ONEPENNY = 'однокопеечных марок'
+  TWOPENNY = 'двухкопеечных марок'
+  THREEPENNY = 'трехкопеечных марок'
+  FIVEPENNY = 'пятикопеечных марок'
 
-    def sum_of_delivery(price)
-      if price == 1
-        return { first: 1, second: 0 }
-      elsif price.even?
-        return { first: 0, second: price / 2 }
+  class WarmUpLevel
+    def sum_of_delivery(delivery_price)
+      if delivery_price == 1
+        return { one_cent_stamp_amount: 1, two_cent_stamp_amount: 0 }
+      elsif delivery_price.even?
+        return { one_cent_stamp_amount: 0, two_cent_stamp_amount: delivery_price / 2 }
       end
 
-      { first: 1, second: price / 2 }
+      { one_cent_stamp_amount: 1, two_cent_stamp_amount: delivery_price / 2 }
     end
   end
 
-  class FirstLevel
-    THREEPENNY = 'трехкопеечных марок'.freeze
-    FIVEPENNY = 'пятикопеечных марок'.freeze
 
-    def sum_of_delivery(price)
-      price = price.to_i
-      f_count = price / 5
-      f_remain = price % 5
+  class FirstLevel
+    def sum_of_delivery(delivery_price)
+      delivery_price = delivery_price.to_i
+      f_count = delivery_price / 5
+      f_remain = delivery_price % 5
       t_count = f_remain / 3
       t_remain = f_remain % 3
       f_count -= t_remain
       t_count += t_remain * 2
-      { first: t_count, second: f_count }
+      { three_cent_stamp_amount: t_count, five_cent_stamp_amount: f_count }
     end
   end
 
   class CalculatingDelivery
-    def initialize(price, stamp_price)
-      @price = price
+    def initialize(delivery_price, stamp_price)
+      @delivery_price = delivery_price
       @stamp_price = stamp_price
     end
 
     def run
-      @stamp_price.sum_of_delivery(@price)
+      stamp_price.sum_of_delivery(delivery_price)
     end
+
+    private
+
+    attr_reader :delivery_price, :stamp_price
   end
 
   class ResultMessage
-    def message(stamps_value, first_value, second_value)
-      "#{stamps_value[:first]} #{first_value} и #{stamps_value[:second]} #{second_value}"
+    def message_for_warm_up_level(stamps_value, first_value, second_value)
+      "#{stamps_value[:one_cent_stamp_amount]} #{first_value} и #{stamps_value[:two_cent_stamp_amount]} #{second_value}"
+    end
+
+    def message_for_first_level(stamps_value, first_value, second_value)
+     "#{stamps_value[:three_cent_stamp_amount]} #{first_value} и #{stamps_value[:five_cent_stamp_amount]} #{second_value}"
     end
   end
 
   class Program
-    def initialize(count, stamp_price, message_builder)
-      @count = count
+    def initialize(count_price, stamp_price, message_builder)
+      @count_price = count_price
       @stamp_price = stamp_price
       @message_builder = message_builder
     end
 
     def run_for_warm_up_level
-      calculating_delivery = CalculatingDelivery.new(@count, @stamp_price)
-      p @message_builder.message(calculating_delivery.run, WarmUpLevel::ONEPENNY, WarmUpLevel::TWOPENNY)
+      calculating_delivery = CalculatingDelivery.new(count_price, stamp_price)
+      message_builder.message_for_warm_up_level(calculating_delivery.run, ONEPENNY, TWOPENNY)
     end
 
     def run_for_first_level
-      calculating_delivery = CalculatingDelivery.new(@count, @stamp_price)
-      p @message_builder.message(calculating_delivery.run, FirstLevel::THREEPENNY, FirstLevel::FIVEPENNY)
+      calculating_delivery = CalculatingDelivery.new(count_price, stamp_price)
+      message_builder.message_for_first_level(calculating_delivery.run, THREEPENNY, FIVEPENNY)
     end
+
+    private
+
+    attr_reader :count_price, :stamp_price, :message_builder
   end
 end
 
 pr = StampsDelivery::Program.new(ARGV[0].to_i, StampsDelivery::FirstLevel.new, StampsDelivery::ResultMessage.new)
-pr.run_for_first_level
+p pr.run_for_first_level
